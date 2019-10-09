f4125a534b1d63e0f02cc2e8e20dfd1f69c17ece
ahnafatef <ahnafatefkhan@gmail.com>
2019-10-09 09:26:05 +0000

    classwork 5 task 2

--- script.js
+++ script.js
@@ -5,7 +5,17 @@ console.log(h2.textContent)
 
 
 /* Task 2 using plain JavaScript */
+let div2a = document.getElementById('task2a');
 
+
+let btnAddTextToDiv2a = document.getElementById('addText');
+
+btnAddTextToDiv2a.addEventListener('click', ()=>{
+	let elementToAdd = document.createElement("p");
+	let div2aPText = document.createTextNode("Hello world");
+	elementToAdd.appendChild(div2aPText);
+	div2a.appendChild(elementToAdd);
+});
 
 
 /* Task 4 using jQuery */
